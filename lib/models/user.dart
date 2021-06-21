435a88a71abaa3bbdfa968ca3f6bf7276505b00d
brianneBrie <bridgetmukuhi@gmail.com>
2021-06-21 12:42:00 +0200

    Setting up for Firebase on the app

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -5,3 +5,25 @@
  * 3. Installing the SDKS
  * 4.Initializing the firebase platform in your code
  */
+/*
+import 'package:firebase_core/firebase_core.dart';
+class User {
+  final String id;
+  final String profileName;
+  final String username;
+  final String url;
+  final String email;
+  final String bio;
+
+  User({this.id, this.profileName, this.username, this.url, required this.email, this.bio});
+
+  factory User.fromDocument(DocumentSnapshot doc){
+    return User(id: doc.documentID,
+        profileName:doc['displayName'],
+        username: doc['username'],
+        url:doc['photoUrl'],
+        email: doc['email'],
+        bio:doc['bio']);
+  }
+
+}*/
